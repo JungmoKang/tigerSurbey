820f5045d3fb0f2d3de2d282641f380582c105bb
Moya <j.kang@kagunova.com>
2016-08-25 13:58:33 +0900

    Update survey.controller.js

--- server/api/survey/survey.controller.js
+++ server/api/survey/survey.controller.js
@@ -1,11 +1,4 @@
-/**
- * Using Rails-like standard naming convention for endpoints.
- * GET     /things              ->  index
- * POST    /things              ->  create
- * GET     /things/:id          ->  show
- * PUT     /things/:id          ->  update
- * DELETE  /things/:id          ->  destroy
- */
+
 
 'use strict';
 
